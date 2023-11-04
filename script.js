0d3276f8b38aae92b35b897f918b9d0680b82402
colinnnnnnnnnnn <126288452+colinnnnnnnnnnn@users.noreply.github.com>
2023-11-04 16:24:21 +0200

    Add all displayed cells in array and assign function playTurn

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const gameBoard = (function () {
-    let turn = 'x'; 
-    let cells = document.querySelectorAll('.cell');
+    let turn = 'x';
+    let board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
+
+    let displayedBoard = document.querySelector('.board');
+    let rows = displayedBoard.children;
+    let cells = [];
+
+    for (row of rows) {
+        cells.push(row.children[0]);
+        cells.push(row.children[1]);
+        cells.push(row.children[2]);
+    }
+
+    console.log(rows);
+    console.log(cells);
+
 
     const playTurn = (cell) => {
         cell.textContent = turn;
-        turn = turn === 'x' ? 'o' :'x';
+        let temp = turn === 'x' ? 'o' : 'x';
+        turn = temp;
     }
 
     cells.forEach(cell => {
